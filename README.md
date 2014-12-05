1d11661d9211355d74a5d0743fb50bb47ff262ae
lynnaloo <lynnaloo@gmail.com>
2014-12-05 16:29:41 -0500

    client code working, no styling or server code yet

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Fireball the Dragon
 ========
 
 Fireball is a dragon that lives in pretty nice castle, but he needs to be fed his dragon food when he
-gets hungry.
+gets hungry or he will run away.
 
 ## Meteor.js
 
@@ -12,3 +12,13 @@ Meteor is a platform for creating web applications in JavaScript.
 
 Fireball is a simple web application that can be used as learning tool for kids and teens
 learning to write JavaScript.
+
+### Requirements
+
+Fireball the dragon begins the game in a content mood with 10 pieces of food. Fireball
+will take up to 20 pieces before he becomes full. As time
+elapses, Fireball eats some of his food:
+
+* At 5 pieces, he starts to get hungry
+* At 3 pieces, he is very hungry and starts to get angry
+* At 0 pieces, he is very angry and ends the game
--- fireball.js
+++ fireball.js
@@ -1,17 +1,48 @@
 if (Meteor.isClient) {
-  // counter starts at 0
+  // counter starts at 10
   Session.setDefault("counter", 10);
 
-  Template.hello.helpers({
+  var timer = setInterval(function () {
+    // every 20 seconds, subtract 1 from the counter
+    var currentCounter = Session.get("counter");
+    if (currentCounter) {
+      Session.set("counter", currentCounter - 1);
+    } else {
+      clearInterval(timer);
+    }
+  }, 20 * 1000);
+
+  Template.feed.helpers({
     counter: function () {
       return Session.get("counter");
+    },
+    mood: function () {
+      var food = Session.get("counter");
+      if (!food) {
+        return "Your dragon has run away!"
+      }
+
+      var mood = "The dragon is ";
+      if (food <= 3) {
+        return mood + "angry.";
+      } else if (food <= 5) {
+        return mood + "hungry.";
+      } else if (food === 20) {
+        return mood + "full.";
+      } else {
+        return mood + "content.";
+      }
     }
   });
 
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+      var currentCounter = Session.get("counter");
+      if (currentCounter > 0 && currentCounter < 20) {
+        Session.set("counter", currentCounter + 1);
+      }
+      // TODO: alert that dragon is full, or that game is over
     }
   });
 }
